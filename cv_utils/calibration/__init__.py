96e742c543513aef891a7dd85275e00d20eb7039
Paul DeCoursey <paul@decoursey.net>
2020-03-06 09:11:00 -0600

    surfacing error potential

--- cv_utils/calibration/__init__.py
+++ cv_utils/calibration/__init__.py
@@ -7,10 +7,16 @@ import numpy as np
 from cv_utils.calibration.geom import Point2D, Point3D, PointMapping, AxisLabel, Axis, Line
 
 
+__all__ = ["Camera", "Point2D", "Point3D", "PointMapping", "AxisLabel", "Axis", "Line"]
+
+
 @attr.s
 class Camera:
     matrix: typing.List[typing.List[float]] = attr.ib()
     distortion_coefficients: typing.List[float] = attr.ib()
+
+    # @classmethod
+    # def 
 
     def matrix_numpy(self):
         return np.array(self.matrix)
@@ -19,6 +25,7 @@ class Camera:
         return np.array(self.distortion_coefficients)
 
     def calibrate(self, mappings: typing.List[PointMapping]):
+        assert len(mappings) >= 4, "4 or more mappings are required to calibrate"
         object_points = [[p.world.x, p.world.y, p.world.z] for p in mappings]
         image_points = [[p.image.x, p.image.y] for p in mappings]
         result, rvec, tvec = cv.solvePnP(
@@ -30,9 +37,9 @@ class Camera:
             None,
             False,
             cv.SOLVEPNP_ITERATIVE)
-        return {
+        return (result, {
                 'cameraMatrix': self.matrix,
                 'distortionCoefficients': self.distortion_coefficients,
                 'rotationVector': rvec.tolist(),
                 'translationVector': tvec.tolist(),
-            }
+            }, )
