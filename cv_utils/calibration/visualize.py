445a85b61227b7857f738ae7cabc53a3d67a619a
Theodore Quinn <tcquinn@tcquinn.org>
2021-02-12 12:02:24 -0800

    Move Honeycomb functions to Honeycomb module

--- cv_utils/calibration/visualize.py
+++ cv_utils/calibration/visualize.py
@@ -55,7 +55,7 @@ def visualize_calibration(
     client_secret=None
 ):
     logger.info('Launching Honeycomb client')
-    client = generate_client(
+    client = cv_utils.calibration.honeycomb.generate_client(
         client=client,
         uri=uri,
         token_uri=token_uri,
@@ -368,7 +368,7 @@ def overlay_floor_lines(
     client_secret=None
 ):
     logger.info('Launching Honeycomb client')
-    client = generate_client(
+    client = cv_utils.calibration.honeycomb.generate_client(
         client=client,
         uri=uri,
         token_uri=token_uri,
@@ -417,7 +417,7 @@ def overlay_floor_lines(
     logger.info('Fetched {} images'.format(len(metadata)))
     logger.info('Fetching camera calibrations')
     camera_ids = [metadatum['device_id'] for metadatum in metadata]
-    camera_calibrations = fetch_camera_calibrations(
+    camera_calibrations = cv_utils.calibration.honeycomb.fetch_camera_calibrations(
         camera_ids=camera_ids,
         start=visualization_datetime,
         end=visualization_datetime,
@@ -430,7 +430,7 @@ def overlay_floor_lines(
         client_secret=client_secret
     )
     logger.info('Fetching camera names')
-    camera_names = fetch_camera_names(
+    camera_names = cv_utils.calibration.honeycomb.fetch_camera_names(
         camera_ids=camera_ids,
         chunk_size=chunk_size,
         client=client,
@@ -646,303 +646,3 @@ def generate_grid_corners(room_corners):
         [float(math.floor(room_corners[1, 0])), float(math.floor(room_corners[1, 1]))],
     ])
     return grid_corners
-
-def fetch_camera_names(
-    camera_ids,
-    chunk_size=100,
-    client=None,
-    uri=None,
-    token_uri=None,
-    audience=None,
-    client_id=None,
-    client_secret=None
-):
-    client = generate_client(
-        client=client,
-        uri=uri,
-        token_uri=token_uri,
-        audience=audience,
-        client_id=client_id,
-        client_secret=client_secret
-    )
-    logger.info('Fetching camera names for specified camera device IDs')
-    result = client.bulk_query(
-        request_name='searchDevices',
-        arguments={
-            'query': {
-                'type': 'QueryExpression!',
-                'value': {
-                    'field': 'device_id',
-                    'operator': 'IN',
-                    'values': camera_ids
-                }
-            }
-        },
-        return_data=[
-            'device_id',
-            'name'
-        ],
-        id_field_name = 'device_id',
-        chunk_size=chunk_size
-    )
-    camera_names = {device.get('device_id'): device.get('name') for device in result}
-    logger.info('Fetched {} camera names'.format(len(camera_names)))
-    return camera_names
-
-def fetch_camera_calibrations(
-    camera_ids,
-    start=None,
-    end=None,
-    chunk_size=100,
-    client=None,
-    uri=None,
-    token_uri=None,
-    audience=None,
-    client_id=None,
-    client_secret=None
-):
-    intrinsic_calibrations = fetch_intrinsic_calibrations(
-        camera_ids=camera_ids,
-        start=start,
-        end=end,
-        chunk_size=chunk_size,
-        client=None,
-        uri=None,
-        token_uri=None,
-        audience=None,
-        client_id=None,
-        client_secret=None
-    )
-    extrinsic_calibrations = fetch_extrinsic_calibrations(
-        camera_ids=camera_ids,
-        start=start,
-        end=end,
-        chunk_size=chunk_size,
-        client=None,
-        uri=None,
-        token_uri=None,
-        audience=None,
-        client_id=None,
-        client_secret=None
-    )
-    camera_calibrations = dict()
-    for camera_id in camera_ids:
-        if camera_id not in intrinsic_calibrations.keys():
-            logger.warning('No intrinsic calibration found for camera ID {}'.format(
-                camera_id
-            ))
-            continue
-        if camera_id not in extrinsic_calibrations.keys():
-            logger.warning('No extrinsic calibration found for camera ID {}'.format(
-                camera_id
-            ))
-            continue
-        camera_calibrations[camera_id] = {**intrinsic_calibrations[camera_id], **extrinsic_calibrations[camera_id]}
-    return camera_calibrations
-
-def fetch_intrinsic_calibrations(
-    camera_ids,
-    start=None,
-    end=None,
-    chunk_size=100,
-    client=None,
-    uri=None,
-    token_uri=None,
-    audience=None,
-    client_id=None,
-    client_secret=None
-):
-    client = generate_client(
-        client=client,
-        uri=uri,
-        token_uri=token_uri,
-        audience=audience,
-        client_id=client_id,
-        client_secret=client_secret
-    )
-    logger.info('Fetching intrinsic calibrations for specified camera device IDs and time span')
-    result = client.bulk_query(
-        request_name='searchIntrinsicCalibrations',
-        arguments={
-            'query': {
-                'type': 'QueryExpression!',
-                'value': {
-                    'field': 'device',
-                    'operator': 'IN',
-                    'values': camera_ids
-                }
-            }
-        },
-        return_data=[
-            'intrinsic_calibration_id',
-            'start',
-            'end',
-            {'device': [
-                'device_id'
-            ]},
-            'camera_matrix',
-            'distortion_coefficients',
-            'image_width',
-            'image_height'
-        ],
-        id_field_name = 'intrinsic_calibration_id',
-        chunk_size=chunk_size
-    )
-    logger.info('Fetched {} intrinsic calibrations for specified camera IDs'.format(len(result)))
-    filtered_result = minimal_honeycomb.filter_assignments(
-        result,
-        start,
-        end
-    )
-    logger.info('{} intrinsic calibrations are consistent with specified start and end times'.format(len(filtered_result)))
-    intrinsic_calibrations = dict()
-    for datum in filtered_result:
-        camera_id = datum.get('device').get('device_id')
-        if camera_id in intrinsic_calibrations.keys():
-            raise ValueError('More than one intrinsic calibration found for camera {}'.format(
-                camera_id
-            ))
-        intrinsic_calibrations[camera_id] = {
-            'camera_matrix': np.asarray(datum.get('camera_matrix')),
-            'distortion_coefficients': np.asarray(datum.get('distortion_coefficients')),
-            'image_width': datum.get('image_width'),
-            'image_height': datum.get('image_height')
-        }
-    return intrinsic_calibrations
-
-def fetch_extrinsic_calibrations(
-    camera_ids,
-    start=None,
-    end=None,
-    chunk_size=100,
-    client=None,
-    uri=None,
-    token_uri=None,
-    audience=None,
-    client_id=None,
-    client_secret=None
-):
-    client = generate_client(
-        client=client,
-        uri=uri,
-        token_uri=token_uri,
-        audience=audience,
-        client_id=client_id,
-        client_secret=client_secret
-    )
-    logger.info('Fetching extrinsic calibrations for specified camera device IDs and time span')
-    result = client.bulk_query(
-        request_name='searchExtrinsicCalibrations',
-        arguments={
-            'query': {
-                'type': 'QueryExpression!',
-                'value': {
-                    'field': 'device',
-                    'operator': 'IN',
-                    'values': camera_ids
-                }
-            }
-        },
-        return_data=[
-            'extrinsic_calibration_id',
-            'start',
-            'end',
-            {'device': [
-                'device_id'
-            ]},
-            {'coordinate_space': [
-                'space_id'
-            ]},
-            'translation_vector',
-            'rotation_vector'
-        ],
-        id_field_name = 'extrinsic_calibration_id',
-        chunk_size=chunk_size
-    )
-    logger.info('Fetched {} extrinsic calibrations for specified camera IDs'.format(len(result)))
-    filtered_result = minimal_honeycomb.filter_assignments(
-        result,
-        start,
-        end
-    )
-    logger.info('{} extrinsic calibrations are consistent with specified start and end times'.format(len(filtered_result)))
-    extrinsic_calibrations = dict()
-    space_ids = list()
-    for datum in filtered_result:
-        camera_id = datum.get('device').get('device_id')
-        space_id = datum.get('coordinate_space').get('space_id')
-        space_ids.append(space_id)
-        if camera_id in extrinsic_calibrations.keys():
-            raise ValueError('More than one extrinsic calibration found for camera {}'.format(
-                camera_id
-            ))
-        extrinsic_calibrations[camera_id] = {
-            'space_id': space_id,
-            'rotation_vector': np.asarray(datum.get('rotation_vector')),
-            'translation_vector': np.asarray(datum.get('translation_vector'))
-        }
-    if len(np.unique(space_ids)) > 1:
-        raise ValueError('More than one coordinate space found among fetched calibrations')
-    return extrinsic_calibrations
-
-def fetch_camera_device_id_lookup(
-    assignment_ids,
-    client=None,
-    uri=None,
-    token_uri=None,
-    audience=None,
-    client_id=None,
-    client_secret=None
-):
-    client = generate_client(
-        client=client,
-        uri=uri,
-        token_uri=token_uri,
-        audience=audience,
-        client_id=client_id,
-        client_secret=client_secret
-    )
-    result = client.bulk_query(
-        request_name='searchAssignments',
-        arguments={
-            'query': {
-                'type': 'QueryExpression!',
-                'value': {
-                    'field': 'assignment_id',
-                    'operator': 'IN',
-                    'values': assignment_ids
-                }
-        }},
-        return_data=[
-            'assignment_id',
-            {'assigned': [
-                {'... on Device': [
-                    'device_id'
-                ]}
-            ]}
-        ],
-        id_field_name='assignment_id'
-    )
-    camera_device_id_lookup = dict()
-    for datum in result:
-        camera_device_id_lookup[datum.get('assignment_id')] = datum.get('assigned').get('device_id')
-    return camera_device_id_lookup
-
-
-def generate_client(
-    client=None,
-    uri=None,
-    token_uri=None,
-    audience=None,
-    client_id=None,
-    client_secret=None
-):
-    if client is None:
-        client=minimal_honeycomb.MinimalHoneycombClient(
-            uri=uri,
-            token_uri=token_uri,
-            audience=audience,
-            client_id=client_id,
-            client_secret=client_secret
-        )
-    return client
