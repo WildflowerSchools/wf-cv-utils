639f103fe9c431d659db16ca3813aa720cbaabf1
Theodore Quinn <tcquinn@tcquinn.org>
2021-12-30 10:22:23 -0800

    Add ability to draw text box (attempt 4)

--- cv_utils/drawing/opencv.py
+++ cv_utils/drawing/opencv.py
@@ -88,7 +88,7 @@ def draw_line(
 
 def draw_text_box(
     original_image,
-    coordinates,
+    anchor_coordinates,
     text,
     horizontal_alignment='center',
     vertical_alignment='bottom',
@@ -102,45 +102,26 @@ def draw_text_box(
     box_fill=True,
     box_alpha=1.0
 ):
-    thickness = math.ceil(text_line_width)
-    text_box_size, baseline = cv.getTextSize(
+    text_box_coordinates = get_text_box_coordinates(
+        anchor_coordinates=anchor_coordinates,
         text=text,
-        fontFace=font_face,
-        fontScale=font_scale,
-        thickness=thickness
+        horizontal_alignment=horizontal_alignment,
+        vertical_alignment=vertical_alignment,
+        font_face=font_face,
+        font_scale=font_scale,
+        line_width=text_line_width
     )
-    text_box_width, text_box_height = text_box_size
-    if horizontal_alignment == 'left':
-        org_u = coordinates[0]
-    elif horizontal_alignment == 'center':
-        org_u = coordinates[0] - text_box_width / 2
-    elif horizontal_alignment == 'right':
-        org_u = coordinates[0] - text_box_width
-    else:
-        raise ValueError('Horizontal aligment \'{}\' not recognized'.format(horizontal_alignment))
-    if vertical_alignment == 'top':
-        org_v = coordinates[1] - (text_box_height + baseline)
-    elif vertical_alignment == 'middle':
-        org_v = coordinates[1] - (text_box_height + baseline) / 2
-    elif vertical_alignment == 'bottom':
-        org_v = coordinates[1]
-    else:
-        raise ValueError('Vertical aligment \'{}\' not recognized'.format(vertical_alignment))
-    rectangle_coordinates = [
-        [org_u, org_v],
-        [org_u + text_box_width, org_v + text_box_height]
-    ]
     new_image = draw_rectangle(
-        original_image,
-        coordinates=rectangle_coordinates,
+        original_image=original_image,
+        coordinates=text_box_coordinates,
         line_width=box_line_width,
         color=box_color,
         fill=box_fill,
         alpha=box_alpha
     )
     new_image = draw_text(
-        new_image,
-        coordinates=coordinates,
+        original_image=new_image,
+        anchor_coordinates=anchor_coordinates,
         text=text,
         horizontal_alignment=horizontal_alignment,
         vertical_alignment=vertical_alignment,
@@ -154,7 +135,7 @@ def draw_text_box(
 
 def draw_text(
     original_image,
-    coordinates,
+    anchor_coordinates,
     text,
     horizontal_alignment='center',
     vertical_alignment='bottom',
@@ -164,39 +145,24 @@ def draw_text(
     color='#00ff00',
     alpha=1.0
 ):
-    for coordinate in coordinates:
-        if abs(coordinate) > 2**30:
+    for anchor_coordinate in anchor_coordinates:
+        if abs(anchor_coordinate) > 2**30:
             return original_image
     color_bgr = cv_utils.color.hex_to_bgr(color)
-    thickness = math.ceil(line_width)
-    text_box_size, baseline = cv.getTextSize(
+    text_org_u, text_org_v = get_text_org(
+        anchor_coordinates=anchor_coordinates,
         text=text,
-        fontFace=font_face,
-        fontScale=font_scale,
-        thickness=thickness
+        horizontal_alignment=horizontal_alignment,
+        vertical_alignment=vertical_alignment,
+        font_face=font_face,
+        font_scale=font_scale,
+        line_width=line_width
     )
-    text_box_width, text_box_height = text_box_size
-    if horizontal_alignment == 'left':
-        org_u = coordinates[0]
-    elif horizontal_alignment == 'center':
-        org_u = coordinates[0] - text_box_width / 2
-    elif horizontal_alignment == 'right':
-        org_u = coordinates[0] - text_box_width
-    else:
-        raise ValueError('Horizontal aligment \'{}\' not recognized'.format(horizontal_alignment))
-    if vertical_alignment == 'top':
-        org_v = coordinates[1] + text_box_height
-    elif vertical_alignment == 'middle':
-        org_v = coordinates[1] + text_box_height / 2
-    elif vertical_alignment == 'bottom':
-        org_v = coordinates[1]
-    else:
-        raise ValueError('Vertical aligment \'{}\' not recognized'.format(vertical_alignment))
     overlay_image = original_image.copy()
     overlay_image = cv.putText(
         img=overlay_image,
         text=text,
-        org=(int(round(org_u)), int(round(org_v))),
+        org=(int(round(text_org_u)), int(round(text_org_v))),
         fontFace=font_face,
         fontScale=font_scale,
         color=color_bgr,
@@ -212,6 +178,79 @@ def draw_text(
     )
     return new_image
 
+def get_text_box_coordinates(
+    anchor_coordinates,
+    text,
+    horizontal_alignment,
+    vertical_alignment,
+    font_face=cv.FONT_HERSHEY_PLAIN,
+    font_scale=1.0,
+    line_width=1
+):
+    text_org_u, text_org_v = get_text_org(
+        anchor_coordinates=anchor_coordinates,
+        text=text,
+        horizontal_alignment=horizontal_alignment,
+        vertical_alignment=vertical_alignment,
+        font_face=font_face,
+        font_scale=font_scale,
+        line_width=line_width
+    )
+    thickness = math.ceil(line_width)
+    text_size, baseline = cv.getTextSize(
+        text=text,
+        fontFace=font_face,
+        fontScale=font_scale,
+        thickness=thickness
+    )
+    text_width, text_height = text_size
+    baseline += thickness
+    text_box_lower_left_u = text_org_u
+    text_box_lower_left_v  = text_org_v + baseline
+    text_box_upper_right_u = text_org_u + text_width
+    text_box_upper_right_v = text_org_v - text_height
+    coordinates = (
+        (text_box_lower_left_u, text_box_lower_left_v),
+        (text_box_upper_right_u, text_box_upper_right_v)
+    )
+    return coordinates
+
+def get_text_org(
+    anchor_coordinates,
+    text,
+    horizontal_alignment,
+    vertical_alignment,
+    font_face=cv.FONT_HERSHEY_PLAIN,
+    font_scale=1.0,
+    line_width=1
+):
+    thickness = math.ceil(line_width)
+    text_size, baseline = cv.getTextSize(
+        text=text,
+        fontFace=font_face,
+        fontScale=font_scale,
+        thickness=thickness
+    )
+    text_width, text_height = text_size
+    baseline += thickness
+    anchor_coordinates_u, anchor_coordinates_v = anchor_coordinates
+    if horizontal_alignment == 'left':
+        text_org_u = anchor_coordinates_u
+    elif horizontal_alignment == 'center':
+        text_org_u = anchor_coordinates_u - text_width / 2
+    elif horizontal_alignment == 'right':
+        text_org_u = anchor_coordinates_u - text_width
+    else:
+        raise ValueError('Horizontal aligment \'{}\' not recognized'.format(horizontal_alignment))
+    if vertical_alignment == 'top':
+        text_org_v = anchor_coordinates_v + text_height
+    elif vertical_alignment == 'middle':
+        text_org_v = canchor_oordinates_v + text_height / 2
+    elif vertical_alignment == 'bottom':
+        text_org_v = anchor_coordinates_v
+    else:
+        raise ValueError('Vertical aligment \'{}\' not recognized'.format(vertical_alignment))
+    return (text_org_u, text_org_v)
 
 def draw_point(
     original_image,
