62d92294750914005cbab89fffe8f72face05069
Theodore Quinn <tcquinn@tcquinn.org>
2022-06-15 13:52:00 -0700

    Implementbasic ChArUco board creation function

--- cv_utils/__init__.py
+++ cv_utils/__init__.py
@@ -3,3 +3,4 @@ from cv_utils.color import *
 from cv_utils.video import *
 # from cv_utils.drawing.matplotlib import *
 from cv_utils.drawing.opencv import *
+from cv_utils.aruco import *
---
+++ cv_utils/aruco.py
@@ -0,0 +1,76 @@
+import cv2 as cv
+
+ARUCO_DICTIONARIES = {
+    cv.aruco.DICT_4X4_50: {'marker_size': 4, 'num_markers': 50, 'april_tag': False},
+    cv.aruco.DICT_4X4_100: {'marker_size': 4, 'num_markers': 100, 'april_tag': False},
+    cv.aruco.DICT_4X4_250: {'marker_size': 4, 'num_markers': 250, 'april_tag': False},
+    cv.aruco.DICT_4X4_1000: {'marker_size': 4, 'num_markers': 1000, 'april_tag': False},
+    cv.aruco.DICT_5X5_50: {'marker_size': 5, 'num_markers': 50, 'april_tag': False},
+    cv.aruco.DICT_5X5_100: {'marker_size': 5, 'num_markers': 100, 'april_tag': False},
+    cv.aruco.DICT_5X5_250: {'marker_size': 5, 'num_markers': 250, 'april_tag': False},
+    cv.aruco.DICT_5X5_1000: {'marker_size': 5, 'num_markers': 1000, 'april_tag': False},
+    cv.aruco.DICT_6X6_50: {'marker_size': 6, 'num_markers': 50, 'april_tag': False},
+    cv.aruco.DICT_6X6_100: {'marker_size': 6, 'num_markers': 100, 'april_tag': False},
+    cv.aruco.DICT_6X6_250: {'marker_size': 6, 'num_markers': 250, 'april_tag': False},
+    cv.aruco.DICT_6X6_1000: {'marker_size': 6, 'num_markers': 1000, 'april_tag': False},
+    cv.aruco.DICT_7X7_50: {'marker_size': 7, 'num_markers': 50, 'april_tag': False},
+    cv.aruco.DICT_7X7_100: {'marker_size': 7, 'num_markers': 100, 'april_tag': False},
+    cv.aruco.DICT_7X7_250: {'marker_size': 7, 'num_markers': 250, 'april_tag': False},
+    cv.aruco.DICT_7X7_1000: {'marker_size': 7, 'num_markers': 1000, 'april_tag': False},
+    cv.aruco.DICT_ARUCO_ORIGINAL: {'marker_size': 5, 'num_markers': 1024, 'april_tag': False},
+    cv.aruco.DICT_APRILTAG_16h5: {'marker_size': 4, 'num_markers': 30, 'april_tag': True},
+    cv.aruco.DICT_APRILTAG_25h9: {'marker_size': 5, 'num_markers': 35, 'april_tag': True},
+    cv.aruco.DICT_APRILTAG_36h10: {'marker_size': 6, 'num_markers': 2320, 'april_tag': True},
+    cv.aruco.DICT_APRILTAG_36h11: {'marker_size': 6, 'num_markers': 587, 'april_tag': True}
+}
+
+def create_charuco_board(
+    num_squares_x=7,
+    num_squares_y=5,
+    marker_size=6,
+    april_tag=False,
+    square_side_length=1.0,
+    marker_side_length_ratio=0.8
+):
+    aruco_dict = fetch_aruco_dictionary(
+        num_squares_x=num_squares_x,
+        num_squares_y=num_squares_y,
+        marker_size=marker_size,
+        april_tag=april_tag
+    )
+    marker_side_length = marker_side_length_ratio*square_side_length
+    board = cv.aruco.CharucoBoard_create(
+        squaresX=num_squares_x,
+        squaresY=num_squares_y,
+        squareLength=square_side_length,
+        markerLength=marker_side_length,
+        dictionary=aruco_dict
+    )
+    return board
+
+def fetch_aruco_dictionary(
+    num_squares_x=7,
+    num_squares_y=5,
+    marker_size=6,
+    april_tag=False
+):
+    compatible_aruco_dictionaries = list(filter(
+        lambda item: (
+            item[1]['marker_size']==marker_size and
+            item[1]['num_markers'] >= num_squares_x*num_squares_y and
+            item[1]['april_tag'] == april_tag
+        ),
+        ARUCO_DICTIONARIES.items()
+    ))
+    if len(compatible_aruco_dictionaries) == 0:
+        raise ValueError('No predefined Aruco dictionaries are compatible with the specified parameters')
+    elif len(compatible_aruco_dictionaries) == 1:
+        selected_aruco_dictionary = compatible_aruco_dictionaries[0]
+    else:
+        selected_aruco_dictionary = min(
+            compatible_aruco_dictionaries,
+            key=lambda x: x[1]['num_markers']
+        )
+    aruco_dict_specifier = selected_aruco_dictionary[0]
+    aruco_dictionary = cv.aruco.Dictionary_get(aruco_dict_specifier)
+    return aruco_dictionary
