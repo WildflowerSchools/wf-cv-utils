249adc908a59b72f39b318375a6bf48e305dbee2
Theodore Quinn <tcquinn@tcquinn.org>
2023-07-16 18:08:41 -0500

    Add fetch_colmap_camera_data_local functionality

--- cv_utils/colmap.py
+++ cv_utils/colmap.py
@@ -105,4 +105,225 @@ def fetch_colmap_image_data_local(
     )
     return df
 
+def fetch_colmap_camera_data_local(
+    calibration_directory=None,
+    calibration_identifier=None,
+    path=None
+):
+    """
+    Fetches data from COLMAP cameras output file and assembles into dataframe.
 
+    The script parses the COLMAP cameras output file, extracting the COLMAP
+    camera ID, COLMAP camera model (e.g., OPENCV), image width, image height,
+    and intrinsic calibration parameters for each camera.
+
+    For each camera, it then extracts the camera matrix and distortion
+    coefficients from the intrinsic calibration parameters.
+
+    By default, the script assumes that the COLMAP cameras output is in a file
+    called cameras.txt in the directory
+    calibration_directory/calibration_identifier. These are the also the default
+    path and naming conventions for COLMAP. Alternatively, the user can
+    explicitly specify the path for the COLMAP cameras output file.
+
+    Args:
+        calibration_directory (str): Path to directory containing calibrations
+        calibration_identifier (str): Identifier for this particular calibration
+        path (str): Explicit path for COLMAP cameras output file (default is
+        None)
+
+    Returns:
+        (DataFrame) Dataframe containing camera data
+    """
+    if path is None:
+        if calibration_directory is None or calibration_identifier is None:
+            raise ValueError('Must specify either camera data path or calibration directory and calibration identifier')
+        path = os.path.join(
+            calibration_directory,
+            calibration_identifier,
+            'cameras.txt'
+        )
+    cameras=list()
+    with open(path, 'r') as fp:
+        for line_index, line in enumerate(fp):
+            if len(line) == 0 or line[0] == '#':
+                continue
+            word_list = line.split()
+            if len(word_list) < 5:
+                raise ValueError('Line {} is shorter than expected: {}'.format(
+                    line_index,
+                    line
+                ))
+            camera = {
+                'colmap_camera_id': int(word_list[0]),
+                'colmap_camera_model': word_list[1],
+                'image_width': int(word_list[2]),
+                'image_height': int(word_list[3]),
+                'colmap_parameters': np.asarray([float(parameter_string) for parameter_string in word_list[4:]])
+            }
+            cameras.append(camera)
+    df = pd.DataFrame.from_records(cameras)
+    df['camera_matrix'] = df.apply(
+        lambda row: colmap_parameters_to_opencv_parameters(
+            row['colmap_parameters'],
+            row['colmap_camera_model']
+        )[0],
+        axis=1
+    )
+    df['distortion_coefficients'] = df.apply(
+        lambda row: colmap_parameters_to_opencv_parameters(
+            row['colmap_parameters'],
+            row['colmap_camera_model']
+        )[1],
+        axis=1
+    )
+    df = df.astype({
+        'colmap_camera_id': 'int',
+        'colmap_camera_model': 'string',
+        'image_width': 'int',
+        'image_height': 'int',
+        'colmap_parameters': 'object',
+        'camera_matrix': 'object',
+        'distortion_coefficients': 'object'
+    })
+    df.set_index('colmap_camera_id', inplace=True)
+    df = (
+        df
+        .reindex(columns=[
+            'colmap_camera_id',
+            'colmap_camera_model',
+            'image_width',
+            'image_height',
+            'colmap_parameters',
+            'camera_matrix',
+            'distortion_coefficients'
+        ])
+        .set_index('colmap_camera_id')
+    )
+    return df
+
+def colmap_parameters_to_opencv_parameters(colmap_parameters, colmap_camera_model):
+    if colmap_camera_model == 'SIMPLE_PINHOLE':
+        fx = colmap_parameters[0]
+        fy = colmap_parameters[0]
+        cx = colmap_parameters[1]
+        cy = colmap_parameters[2]
+        distortion_coefficients = None
+    elif colmap_camera_model == 'PINHOLE':
+        fx = colmap_parameters[0]
+        fy = colmap_parameters[1]
+        cx = colmap_parameters[2]
+        cy = colmap_parameters[3]
+        distortion_coefficients = None
+    elif colmap_camera_model == 'SIMPLE_RADIAL':
+        fx = colmap_parameters[0]
+        fy = colmap_parameters[0]
+        cx = colmap_parameters[1]
+        cy = colmap_parameters[2]
+        distortion_coefficients = np.array([
+            colmap_parameters[3],
+            0.0,
+            0.0,
+            0.0
+        ])
+    elif colmap_camera_model == 'RADIAL':
+        fx = colmap_parameters[0]
+        fy = colmap_parameters[0]
+        cx = colmap_parameters[1]
+        cy = colmap_parameters[2]
+        distortion_coefficients = np.array([
+            colmap_parameters[3],
+            colmap_parameters[4],
+            0.0,
+            0.0
+        ])
+    elif colmap_camera_model == 'OPENCV':
+        fx = colmap_parameters[0]
+        fy = colmap_parameters[1]
+        cx = colmap_parameters[2]
+        cy = colmap_parameters[3]
+        distortion_coefficients = np.array([
+            colmap_parameters[4],
+            colmap_parameters[5],
+            colmap_parameters[6],
+            colmap_parameters[7]
+        ])
+    elif colmap_camera_model == 'OPENCV_FISHEYE':
+        fx = colmap_parameters[0]
+        fy = colmap_parameters[1]
+        cx = colmap_parameters[2]
+        cy = colmap_parameters[3]
+        distortion_coefficients = np.array([
+            colmap_parameters[4],
+            colmap_parameters[5],
+            0.0,
+            0.0,
+            colmap_parameters[6],
+            colmap_parameters[7],
+            0.0,
+            0.0
+        ])
+    elif colmap_camera_model == 'FULL_OPENCV':
+        fx = colmap_parameters[0]
+        fy = colmap_parameters[1]
+        cx = colmap_parameters[2]
+        cy = colmap_parameters[3]
+        distortion_coefficients = np.array([
+            colmap_parameters[4],
+            colmap_parameters[5],
+            colmap_parameters[6],
+            colmap_parameters[7],
+            colmap_parameters[8],
+            colmap_parameters[9],
+            colmap_parameters[10],
+            colmap_parameters[11]
+        ])
+    elif colmap_camera_model == 'SIMPLE_RADIAL_FISHEYE':
+        fx = colmap_parameters[0]
+        fy = colmap_parameters[0]
+        cx = colmap_parameters[1]
+        cy = colmap_parameters[2]
+        distortion_coefficients = np.array([
+            colmap_parameters[3],
+            0.0,
+            0.0,
+            0.0
+        ])
+    elif colmap_camera_model == 'RADIAL_FISHEYE':
+        fx = colmap_parameters[0]
+        fy = colmap_parameters[0]
+        cx = colmap_parameters[1]
+        cy = colmap_parameters[2]
+        distortion_coefficients = np.array([
+            colmap_parameters[3],
+            colmap_parameters[4],
+            0.0,
+            0.0
+        ])
+    elif colmap_camera_model == 'THIN_PRISM_FISHEYE':
+        fx = colmap_parameters[0]
+        fy = colmap_parameters[1]
+        cx = colmap_parameters[2]
+        cy = colmap_parameters[3]
+        distortion_coefficients = np.array([
+            colmap_parameters[4],
+            colmap_parameters[5],
+            colmap_parameters[6],
+            colmap_parameters[7],
+            colmap_parameters[8],
+            colmap_parameters[9],
+            0.0,
+            0.0,
+            colmap_parameters[10],
+            colmap_parameters[11],
+            0.0,
+            0.0
+        ])
+    else:
+        raise ValueError('Camera model {} not found'.format(colmap_camera_model))
+    camera_matrix = np.array([
+        [fx, 0.0, cx],
+        [0.0, fy, cy],
+        [0.0, 0.0, 1.0]
+    ])
+    return camera_matrix, distortion_coefficients
